6bc7b39c13635f8edd8f41a928fbdc1ff7a4e6fd
Ovid <curtis_ovid_poe@yahoo.com>
2013-11-18 12:56:10 +0100

    Add a method we forgot.

--- t/lib/TestsFor/Basic/Subclass.pm
+++ t/lib/TestsFor/Basic/Subclass.pm
@@ -2,6 +2,12 @@ use mop;
 class TestsFor::Basic::Subclass extends TestsFor::Basic {
     use Test::Most;
 
+    method test_this_baby($report) {
+        my $class = $self->test_class;
+        pass "This should run before my parent method ($class)";
+        $self->next::method($report);
+    }
+
     method test_me {
         my $class = $self->test_class;
         ok 1, "I overrode my parent! ($class)";
